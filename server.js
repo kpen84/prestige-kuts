c23af3d4fe066ba14d2529ac45c0b48961e26710
kpen84 <kpen84@gmail.com>
2023-07-15 21:20:20 -0400

    last commit failed

--- server.js
+++ server.js
@@ -1,66 +1,39 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
-const  mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-
-const mongoString = process.env.DATABASE_URL
 mongoose.connect(mongoString);
+const database = mongoose.connection;
 
-const database = mongoose.connection
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
 
 app.use(express.static('public'));
 
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/book.html')
 })
 
-mongoose.connect('mongodb+srv://kpen84:Jessi2464!@api.v2y8zt4.mongodb.net/API', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+//router
 
-app.use(express.json())
+const routes = require('./routes/routes');
+app.use('/api', routes)
 
-const bookingRouter = require('./routes/book')
-app.use('/book', bookingRouter)
 
-const { Schema } = mongoose;
 
-const bookingSchema = new Schema({
-  barber: String,
-  customer: String,
-  apptype: String,
-  email: String,
-  date: Date,
-});
-
-const booking = mongoose.model('book', bookingSchema);
-
-app.post('/', (req, res) => {
-    const bookingData = req.body;
-  
-    const newBooking = new booking({
-      barber: bookingData.barber,
-      customer: bookingData.customer,
-      email: bookingData.email,
-      apptype: bookingData.apptype,
-      date: bookingData.date,
-    });
-  
-    newBooking.save((err, savedBooking) => {
-      if (err) {
-        console.error('Error saving booking:', err);
-        res.status(500).send('Error saving booking');
-      } else {
-        res.status(201).json(savedBooking);
-      }
-    });
-  });
-
-app.listen(3000, () => console.log('Server running on localhost:3000'))
+  app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
