ae617bf1191fa46b61aa471b8a00c121718d0e76
kpen84 <kpen84@gmail.com>
2023-07-17 01:23:15 -0400

    reverted to commit b8026a5 on stub branch

---
+++ models/model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    barber: {
+        required: true,
+        type: String
+    },
+    customer: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    apptype: {
+        required: true,
+        type: String
+    },
+    date: {
+        required: true,
+        type: Date
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
