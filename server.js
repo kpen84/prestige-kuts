63e1dea7b0e8009db0fc17a5c65e7afa0dd3eb92
kpen84 <kpen84@gmail.com>
2023-07-16 22:37:47 -0400

    installed ext. that changed format

--- server.js
+++ server.js
@@ -8,32 +8,31 @@ mongoose.connect(mongoString);
 const database = mongoose.connection;
 
 database.on('error', (error) => {
-    console.log(error)
-})
+    console.log(error);
+});
 
 database.once('connected', () => {
     console.log('Database Connected');
-})
+});
+
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static('public'));
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/book.html')
-})
+    res.sendFile(__dirname + '/public/book.html');
+});
 
 //router
 
 const routes = require('./routes/routes');
-app.use('/api', routes)
+app.use('/api', routes);
 
-
-
-  app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`);
+});
