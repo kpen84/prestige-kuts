2c9bea8efe78a02fb7ddc4b01e9da28e80a907e7
Kody <kpen84@gmail.com>
2023-07-20 21:38:26 -0400

    re-commit

--- public/index.js
+++ public/index.js
@@ -57,20 +57,38 @@ function showSlides(n) {
 
 // Form Data
 
-document.getElementById('bookingForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+document.addEventListener(function () {
+    document.getElementById('bookingForm').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    const barber = document.querySelector('.barber').value;
-    const customer = document.querySelector('.customer').value;
-    const apptype = document.querySelector('.apptype').value;
-    const email = document.querySelector('.email').value;
-    const date = document.getElementById('date').value;
+        const barber = document.querySelector('.barber').value;
+        // const customer = document.querySelector('.customer').value;
+        // const apptype = document.querySelector('.apptype').value;
+        // const email = document.querySelector('.email').value;
+        // const date = document.getElementById('date').value;
 
-    const data = {
-        barber,
-        customer,
-        apptype,
-        email,
-        date,
-    };
+        const data = {
+            barber,
+            // customer,
+            // apptype,
+            // email,
+            // date,
+        };
+
+        fetch('/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+            })
+            .catch((error) => {
+                console.error(error);
+                //
+            });
+    });
 });
--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-mongoose.connect(mongoString);
+mongoose
+    .connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database Connected');
+    })
+    .catch((error) => {
+        console.error('Failed to connect to MongoDB:', error);
+    });
+
 const database = mongoose.connection;
 database.on('error', (error) => {
-    console.log(error);
+    console.error('MongoDB connection error:', error);
 });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//router
-
 const appointmentRouter = require('./router/appointment-book');
-app.use('/', appointmentRouter);
+app.use('/api', appointmentRouter);
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`);
-});
-database.once('connected', () => {
-    console.log('Database Connected');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server Started at http://localhost:${port}`);
 });
