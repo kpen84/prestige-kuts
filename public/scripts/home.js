7a02253e31e6462d29976c01b93ad2592856a832
Kody <kpen84@gmail.com>
2023-07-20 23:16:57 -0400

    created scripts folder

---
+++ public/scripts/home.js
@@ -0,0 +1,44 @@
+function openNav() {
+    document.getElementById('mySidenav').style.left = '0';
+}
+
+function closeNav() {
+    document.getElementById('mySidenav').style.left = '-1000px';
+}
+
+//slideshow
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        if (!slides[i]) return;
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active');
+    }
+    if (!slides[slideIndex - 1]) return;
+    slides[slideIndex - 1].style.display = 'flex';
+    dots[slideIndex - 1].className += ' active';
+}
